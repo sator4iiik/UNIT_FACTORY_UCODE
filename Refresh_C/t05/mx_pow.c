46fedeb52addac7e1ba9e8d7d47a408a486cf571
Oleksii Reznikov <oreznikov@e1r9p14.unit.ua>
2020-09-18 22:34:53 +0300

    p

---
+++ Refresh_C/t05/mx_pow.c
@@ -0,0 +1,16 @@
+// #include <stdio.h>
+#include <math.h>
+
+double mx_pow(double n, unsigned int pow) {
+    double j=1;
+
+    for(unsigned int i = 1; i <= pow; i++) {
+        j=j*n;
+    }
+    return j;
+}
+// int main() {
+//     double h = 2.5;
+//     unsigned int l = 3;
+//     printf("%.3f v stepeni %d ravno : %.3f\n", h,l, mx_pow(h, l));
+// }
