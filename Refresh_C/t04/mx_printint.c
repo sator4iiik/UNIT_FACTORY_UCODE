cd91b020891ebe14ef38b65f1c9d4ea271ccf680
Oleksii Reznikov <oreznikov@e1r9p14.unit.ua>
2020-09-18 21:24:12 +0300

    k

---
+++ Refresh_C/t04/mx_printint.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void mx_printchar(char c);
+
+void mx_printint(int n) {
+    if (n == -2147483648) {
+        write(1, "-2147483648", 12);
+        return;
+    }
+    if (n<0) {
+        n= -n;
+        mx_printchar('-');
+    }
+    if(n/10 != 0)
+        mx_printint(n/10);
+    mx_printchar(n%10 + 48);
+}
+
+int main() {
+    mx_printint(2147483647);
+    mx_printchar('\n');
+    mx_printint(-2147483648);
+    mx_printchar('\n');
+    mx_printint(0);
+    mx_printchar('\n');
+    mx_printint(-6582);
+    mx_printchar('\n');
+}
