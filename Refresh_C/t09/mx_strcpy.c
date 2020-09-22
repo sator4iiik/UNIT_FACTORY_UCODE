49141ed63ff5219dec2f809c62e45dc9bf6ef83f
Oleksii Reznikov <oreznikov@e1r9p5.unit.ua>
2020-09-22 22:48:24 +0300

    m2m

--- Refresh_C/t09/mx_strcpy.c
+++ Refresh_C/t09/mx_strcpy.c
@@ -1,5 +1,17 @@
-#include <stdio.h>
+// #include <stdio.h>
+// #include <string.h>
 
 char *mx_strcpy(char *dst, const char *src) {
-    
+    char *h = dst;
+    for (int i = 0; src[i] != '\0'; i++) {
+        h[i] = src[i];
+        h[i+1] = '\0';
+    }
+    return h;
 }
+// int main() {
+//     char test[] = "im bored, so wtf?";
+//     char real[] = "Money";
+//     printf("%s\n", mx_strcpy(test, real));
+//     printf("%s\n", strcpy(test, real));
+// }
