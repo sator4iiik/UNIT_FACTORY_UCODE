f78f59a16e82f6400c1b89302042221049185bf3
Oleksii Reznikov <oreznikov@e1r10p7.unit.ua>
2020-10-23 17:08:25 +0300

    adwrf

--- Pathfinder/inc/pathfinder.h
+++ Pathfinder/inc/pathfinder.h
@@ -1,1 +1,18 @@
+#ifndef PATHFINDER
+#define PATHFINDER
 
+#include "libmx.h"
+
+#define INT_MAX 2147483647
+
+#define ERROR_USAGE "usage: ./pathfinder [filename]\n"
+#define ERROR_USAGE "error: file [filename] does not exist"
+
+typedef struct s_finder {
+    int fd;
+}              t_finder;
+
+// Validation of errors
+void mx_wrong_args_or_file(int args, char *argsv[], t_finder *path);
+
+#endif
--- Pathfinder/src/main/main.c
+++ Pathfinder/src/main/main.c
@@ -1,2 +1,10 @@
-#include "libmx.h"
+#include "pathfinder.h"
 
+int main(int argc, char *argv[]) {
+    if (argc == 2) {
+    }
+    else {
+        mx_printerr(ERROR_USAGE);
+    }
+    return 0;
+}
