70c2209ff79a2066d5cdebd04125e668f0f8f437
Oleksii Reznikov <sator4iiik@gmail.com>
2021-04-04 18:06:22 +0300

    Update mx_isalpha.c

--- Marathon_C/Sprint02/t02/mx_isalpha.c
+++ Marathon_C/Sprint02/t02/mx_isalpha.c
@@ -7,3 +7,21 @@ bool mx_isalpha(int c) {
     else
         return false;
 }
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// bool mx_isalpha(int c) {
+//     if (65 <= c && c <= 90)
+//         return true;
+//     else if (97 <= c && c <= 122)
+//         return true;
+//     else
+//         return false;
+
+// }
+
+// int main(void) {
+//     int f = 70;
+//     mx_isalpha(f)?printf("true\n"):printf("false\n");
+// }
