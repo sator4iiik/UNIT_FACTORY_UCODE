6c7e9148c90ce43ceedbc71c62ee9ba20e0650ae
Oleksii Reznikov <sator4iiik@gmail.com>
2021-04-04 18:02:32 +0300

    Update mx_is_positive.c

--- Marathon_C/Sprint02/t00/mx_is_positive.c
+++ Marathon_C/Sprint02/t00/mx_is_positive.c
@@ -1,17 +1,44 @@
+#include <unistd.h>
+
 void mx_printstr(const char *s);
 
 void mx_is_positive(int i) {
-    if (i > 0) {
+    if (i >= 1) {
         mx_printstr("positive");
     }
-    if (i < 0) {
+    else if (i <= -1) {
         mx_printstr("negative");
     }
-    if (i == 0) {
+    else if (i == 0) {
         mx_printstr("zero");
     }
     mx_printstr("\n");
 }
+
+// int main(void) {
+//     mx_is_positive(123);
+//     mx_is_positive(-123);
+//     mx_is_positive(0);
+// }
+
+
+// --------------------------------------
+    
+    
+// void mx_printstr(const char *s);
+
+// void mx_is_positive(int i) {
+//     if (i > 0) {
+//         mx_printstr("positive");
+//     }
+//     if (i < 0) {
+//         mx_printstr("negative");
+//     }
+//     if (i == 0) {
+//         mx_printstr("zero");
+//     }
+//     mx_printstr("\n");
+// }
 
 //int main(void) {
 //    mx_is_positive(5);
