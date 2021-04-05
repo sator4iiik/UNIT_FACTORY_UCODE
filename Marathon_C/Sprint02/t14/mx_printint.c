2990f69bc4b010e7f24527632d6eca596f95ace3
Oleksii Reznikov <sator4iiik@gmail.com>
2021-04-05 20:56:54 +0300

    Update mx_printint.c

--- Marathon_C/Sprint02/t14/mx_printint.c
+++ Marathon_C/Sprint02/t14/mx_printint.c
@@ -3,15 +3,21 @@
 void mx_printchar(char c);
 
 void mx_printint(int n) {
-    if (n<0) {
-        n= -n;
-        mx_printchar('_');
+    if (n == -2147483648) {
+        write(1, "-2147483648", 11);
+    return;
     }
+    
+    if (n < 0) {
+        n= -n;
+        mx_printchar('-');
+    }
+    
     if(n/10 != 0)
         mx_printint(n/10);
-    mx_printchar(n%10 + 48);
+    mx_printchar(n%10 + '0');
 }
 
-//int main() {
-//    mx_printint(2147483647);
-//}
+// int main() {
+//     mx_printint(-214);
+// }
