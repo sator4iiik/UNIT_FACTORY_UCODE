4c6a776defe0e9481973b8387d36b84c58072602
Oleksii Reznikov <sator4iiik@gmail.com>
2021-04-05 21:17:35 +0300

    Update mx_is_odd.c

--- Marathon_C/Sprint02/t01/mx_is_odd.c
+++ Marathon_C/Sprint02/t01/mx_is_odd.c
@@ -17,3 +17,21 @@ bool mx_is_odd(int value) {
 //    bool y = mx_is_odd(8);
 //    printf("i: %i \n", y);
 //}
+
+
+// ------VER-II-------------------------
+
+// #include <stdio.h>
+// #include <stdbool.h>
+
+// bool mx_is_odd(int value) {
+//     if (value % 2 == 0)
+//         return false;
+//     else
+//         return true;
+// }
+
+// int main(void) {
+//     mx_is_odd(-8)?printf("true\n"):printf("false\n");
+//         return 0;
+// }
